7d8349fa930edf0c901fc23f3b21c1c5fba60524
AndreaAspesiPolimi <andrea.aspesi@mail.polimi.it>
2019-12-26 14:50:01 +0100

    Added the Google map  view

--- Implementation/MobileApp/SafeStreets/SafeStreets/StreetsSafetyMapPage.xaml.cs
+++ Implementation/MobileApp/SafeStreets/SafeStreets/StreetsSafetyMapPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace SafeStreets
@@ -15,6 +16,28 @@ namespace SafeStreets
 		public StreetsSafetyMapPage ()
 		{
 			InitializeComponent ();
+
+			Position position = new Position(45.478246, 9.227250);
+			MapSpan mapSpan = MapSpan.FromCenterAndRadius(position, Xamarin.Forms.Maps.Distance.FromKilometers(0.5));
+			xMap.MoveToRegion(mapSpan);
+
+			Pin wharfPin = new Pin
+			{
+				Position = new Position(45.478247, 9.227251),
+				Label = "Titolo",
+				Address = "StoriellaBellaBella",
+				Type = PinType.SavedPin
+			};
+
+			Pin wharfPin2 = new Pin
+			{
+				Position = new Position(45.478245, 9.227251),
+				Label = "Titolo222",
+				Address = "StoriellaBellaBella",
+				Type = PinType.SavedPin
+			};
+			xMap.Pins.Add(wharfPin);
+			xMap.Pins.Add(wharfPin2);
 		}
 	}
 }
