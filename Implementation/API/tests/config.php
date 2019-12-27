172fa5ea01003338fc3f62f52e8293ff6f1a2fe3
ERap320 <battistonelia@erap.space>
2019-12-27 16:33:00 +0100

    Fixed test mysql config file

--- Implementation/API/tests/config.php
+++ Implementation/API/tests/config.php
@@ -2,6 +2,6 @@
   /*DB access parameters declaration for global access*/
   $_CONFIG['host'] = "localhost";
   $_CONFIG['user'] = "root";
-  $_CONFIG['pass'] = "";
+  $_CONFIG['pass'] = "root";
   $_CONFIG['dbname'] = "safestreets";
 ?>
